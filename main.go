548579b6731bee27c068063f2df84f75e50506e1
Roshan Raj <roshanraj@live.com>
2017-10-23 10:58:48 +0530

    added dir traversing and specs.ts file generator

--- main.go
+++ main.go
@@ -3,13 +3,49 @@ import (
 	"fmt"
 	"github.com/spf13/pflag"
 	"os"
-    "path/filepath"	
+	"strings"
+	"path/filepath"	
+	"regexp"
 )
 
 func main(){
 	var file_path = pflag.StringP("flagname","f" ,"./", "path to src")
 	pflag.Parse()
 	ReadDir(*file_path)
+}
+
+func isValidFileToHaveUnitTestCase(file_path string) bool{
+	return !strings.Contains(file_path, "module")
+}
+
+func istypescriptFile(file_path string) bool{
+	matched, err := regexp.MatchString("\\.ts", file_path)
+	specsMatched, serr := regexp.MatchString("\\.specs.ts", file_path)
+	if err == nil && serr == nil{
+		return matched && !specsMatched
+	}else{
+		return false
+	}
+}
+
+func generateSpecsFile(file_path string){
+	var re = regexp.MustCompile("\\.ts")
+	var newFileName = re.ReplaceAllString(file_path, ".specs.ts")
+	fmt.Println("new file  --  ",newFileName)
+	
+	fileInfo, err := os.Stat(newFileName)
+	
+	  // check if there is an error
+	  if err != nil {
+		 // check if error is file does not exist
+		 if os.IsNotExist(err) {
+			fmt.Println("File does not exist.")
+			os.OpenFile(newFileName, os.O_RDONLY|os.O_CREATE, 0666)
+		 }
+	  } else {
+
+	   fmt.Println(fileInfo.Name() , "exists")
+	  }
 }
 
 func ReadDir(searchDir string){
@@ -22,8 +58,19 @@ func ReadDir(searchDir string){
 		})
 		if err == nil {
 			for x, file := range fileList {
-				fmt.Println(x,"  --  ",file)
-				
+				if isValidFileToHaveUnitTestCase(file){
+					if istypescriptFile(file){
+					
+							
+					
+						fmt.Println(x,"  --  ",file, " ts file ")
+						generateSpecsFile(file)
+					}else{
+						fmt.Println(x,"  --  ",file)
+					}
+				}else{
+					fmt.Println(x,"  --  ",file, "  ---  not valid")
+				}
 			}
 		}else{
 			fmt.Println("Oops something broke",err);
