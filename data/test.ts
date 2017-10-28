8f26de8f739acb1b2ee5a890e2ecfb05d3f083bf
rraj <roshanraj@live.com>
2017-10-28 11:48:18 +0530

    file read wip

---
+++ data/test.ts
@@ -0,0 +1,33 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs/Observable";
+import {ICustomerAgreementsState} from "../../domain/state";
+import {CustomerAgreementsActions} from "../../domain/actions";
+import {customerAgreementsQueries} from "../../domain/queries";
+import { OverlayLoaderSize } from "eing-ui-library";
+
+@Component({
+  selector: "eing-customer-agreements-base-component",
+  templateUrl: "./base.component.html",
+  styleUrls: ["./base.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection : ChangeDetectionStrategy.OnPush
+})
+export class CustomerAgreementsBaseComponent implements OnInit {
+
+    public overlayLoaderSize : string = OverlayLoaderSize.PARTIAL;
+    public isLoading$ : Observable<boolean>;
+
+  constructor(private store : Store<ICustomerAgreementsState>,
+          private customerAgreementsActions : CustomerAgreementsActions) {}
+  
+  public ngOnInit(){
+      this.isLoading$ = this.store.select(customerAgreementsQueries.isLoading);
+  }
+  
+  public loadData(){
+      this.store.dispatch(this.customerAgreementsActions.loadAction());
+  }
+
+}
+
--- write_testcases.go
+++ write_testcases.go
@@ -2,15 +2,65 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"regexp"
 )
 
 func getClassName() {
 
 }
-func getPublicFunctions() {
+func getPublicFunctions(line string) {
+	// matched, err := regexp.MatchString("public\\s*[a-zA-Z]+\\([^\\)]*\\)(\\.[^\\)]*\\))?:?", line)
+	re := regexp.MustCompile("public\\s*[a-zA-Z]+\\([^\\)]*\\)(\\.[^\\)]*\\))?:?")
+	match := re.FindStringSubmatch(line)
+	fmt.Println("********")
+	fmt.Println(line)
+	fmt.Println("********")
+	fmt.Println("______________")
+	fmt.Println(match)
+	fmt.Println("______________")
+
+	// if err == nil {
+	// 	fmt.Println(" _________ ", matched)
+	// } else {
+	// 	fmt.Println(" __######___ ", err)
+	// }
 
 }
+
+func ReadFile(path string) {
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	// read file, line by line
+	var text = make([]byte, 1024)
+	for {
+		_, err := file.Read(text)
+		// getPublicFunctions(string(line))
+		// break if finally arrived at end of file
+		if err == io.EOF {
+			break
+		}
+
+		// break if error occured
+		if err != nil && err != io.EOF {
+			if err != err {
+				break
+			}
+		}
+	}
+
+	fmt.Println("==> done reading from file")
+	getPublicFunctions(string(text))
+	// fmt.Println(string(text))
+}
+
 func WriteTestCases(read_file_from, write_file_to string) {
 	fmt.Println("Read test cases to ", read_file_from)
+	ReadFile(read_file_from)
 	fmt.Println("writing test cases to ", write_file_to)
 }
